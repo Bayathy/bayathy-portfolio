4c38671f7a8ad91b312a897e12ce2dcd1adf2580
Bayathy <misosoup0321@icloud.com>
2022-06-12 15:22:40 +0900

    create Space.tsx

---
+++ src/compornent/atom/Space/Space.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { css } from '@emotion/react'
+
+type Space = {
+  space: number
+}
+const Space: React.FC<Space> = ({ space }) => {
+  return (
+    <div
+      css={css`
+        margin: ${space};
+      `}
+    />
+  )
+}
