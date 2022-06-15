41a35f98cf6c791b44f0cdd7d84c6707ea0443c1
Bayathy <misosoup0321@icloud.com>
2022-06-15 10:01:14 +0900

    add PageCotenxtProvider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next'
 import ThemeContextProvider from '../context/ThemeContextProvider/ThemeContextProvider'
 import MainLayout from '../compornent/template/MainLayout/MainLayout'
+import PageContextProvider from '../context/PageContextProvider/PageContextProvider'
 
 const Home: NextPage = () => {
   return (
     <ThemeContextProvider>
-      <MainLayout />
+      <PageContextProvider>
+        <MainLayout />
+      </PageContextProvider>
     </ThemeContextProvider>
   )
 }
