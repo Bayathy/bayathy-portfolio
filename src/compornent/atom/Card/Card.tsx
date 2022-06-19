33f8fcd82ca39b7a6de1620cc2485517a8f256d7
Bayathy <misosoup0321@icloud.com>
2022-06-20 00:54:53 +0900

    rewrite Card.tsx

--- src/compornent/atom/Card/Card.tsx
+++ src/compornent/atom/Card/Card.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { css, useTheme } from '@emotion/react'
+import { css, SerializedStyles, useTheme } from '@emotion/react'
 
 type Card = {
   children: React.ReactNode
+  style: SerializedStyles
 }
 
-const Card: React.FC<Card> = ({ children }) => {
+const Card: React.FC<Card> = ({ children, style }) => {
   const theme = useTheme()
 
   return (
     <div
       css={css`
         background: ${theme.colors.secondary};
+        ${style}
       `}
     >
       {children}
