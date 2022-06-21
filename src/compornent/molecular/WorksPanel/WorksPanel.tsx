4fe5a2cc3c8e7dbc782dc5123585b1e45b098966
Bayathy <misosoup0321@icloud.com>
2022-06-21 09:46:13 +0900

    style WorkPanel

--- src/compornent/molecular/WorksPanel/WorksPanel.tsx
+++ src/compornent/molecular/WorksPanel/WorksPanel.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import { ContentContainer } from '../../atom'
-import { useTheme } from '@emotion/react'
+import { Card, ContentContainer } from '../../atom'
+import { useTheme, css } from '@emotion/react'
 
 export const WorksPanel: React.FC = () => {
   const theme = useTheme()
   return (
     <ContentContainer>
-      <></>
+      <Card
+        style={css`
+          width: 18rem;
+        `}
+      >
+        <div>
+          <div>
+            <p
+              css={css`
+                text-align: center;
+              `}
+            >
+              test
+            </p>
+          </div>
+        </div>
+      </Card>
     </ContentContainer>
   )
 }
