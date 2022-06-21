1ee4832088e9069e06260402a1fe06cbe9e00852
Bayathy <misosoup0321@icloud.com>
2022-06-21 19:04:48 +0900

    add ClassName

--- src/compornent/atom/Card/Card.tsx
+++ src/compornent/atom/Card/Card.tsx
@@ -4,13 +4,15 @@ import { css, SerializedStyles, useTheme } from '@emotion/react'
 type Card = {
   children: React.ReactNode
   style?: SerializedStyles
+  className?: string
 }
 
-export const Card: React.FC<Card> = ({ children, style }) => {
+export const Card: React.FC<Card> = ({ children, style, className }) => {
   const theme = useTheme()
 
   return (
     <div
+      className={className}
       css={css`
         background: ${theme.colors.extra};
         border-radius: 10px;
