d70ca097212f4e85f2058c16b863ebc276280361
Bayathy <misosoup0321@icloud.com>
2022-06-22 12:55:18 +0900

    style WorkPage Image size

--- src/component/organism/WorkDeteal/WorkDeteal.tsx
+++ src/component/organism/WorkDeteal/WorkDeteal.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { ContentContainer } from '../../atom'
 import Image from 'next/image'
+import { css } from '@emotion/react'
+import { ContentContainer } from '../../atom'
 
 export const WorkDeteal: React.FC = () => {
   return (
     <>
       <ContentContainer>
-        <div>
-          <Image src={'/dummy.png'} width={100} height={100} />
+        <div
+          css={css`
+            position: relative;
+            width: 80%;
+            margin: auto;
+            aspect-ratio: 16 /9;
+          `}
+        >
+          <Image src={'/Icon3.jpg'} layout={'fill'} objectFit={'cover'} />
         </div>
       </ContentContainer>
     </>
