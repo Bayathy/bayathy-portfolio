2f84a5250482371527cd8bbd0a1d0a24b59a32dc
Bayathy <misosoup0321@icloud.com>
2022-06-22 23:15:09 +0900

    create WorkdetealCard

---
+++ src/component/molecular/WorkdetealCard/WorkdetealCard.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { css, useTheme } from '@emotion/react'
+import { Card, Space } from '../../atom'
+
+export const WorkdetealCard: React.FC = () => {
+  const theme = useTheme()
+
+  return (
+    <Card
+      style={css`
+        width: 80%;
+        margin: auto;
+        padding: ${theme.padding.small};
+      `}
+    >
+      <p>HackUで制作した作品です。高専生の五人でチームを組みいくつか賞を受賞しました。</p>
+      <Space space={1} margin />
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+        `}
+      >
+        <div
+          css={css`
+            width: max-content;
+            background: ${theme.colors.primary};
+            text-align: center;
+            border-radius: 20px;
+            font-size: 0.8rem;
+          `}
+        >
+          <p
+            css={css`
+              padding: 0.5rem;
+              line-height: 1;
+            `}
+          >
+            技術スタック
+          </p>
+        </div>
+        <p
+          css={css`
+            padding: 0.8rem;
+            line-height: 1;
+          `}
+        >
+          テクノロジー
+        </p>
+      </div>
+    </Card>
+  )
+}
