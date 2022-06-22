c301a99fd4ca2fbcdc20dcae64ca5dcc48da932c
Bayathy <misosoup0321@icloud.com>
2022-06-22 23:15:51 +0900

    update Workdeteal

--- src/component/organism/WorkDeteal/WorkDeteal.tsx
+++ src/component/organism/WorkDeteal/WorkDeteal.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 import { css, useTheme } from '@emotion/react'
-import { Card, ContentContainer, Space, WorkTitle } from '../../atom'
+import { ContentContainer, Space, WorkTitle } from '../../atom'
+import { WorkdetealCard } from '../../molecular'
 
 export const WorkDeteal: React.FC = () => {
   const theme = useTheme()
@@ -29,34 +30,7 @@ export const WorkDeteal: React.FC = () => {
           <WorkTitle />
         </div>
         <Space space={2} margin />
-        <Card
-          style={css`
-            width: 80%;
-            margin: auto;
-            padding: ${theme.padding.small};
-          `}
-        >
-          <p>HackUで制作した作品です。高専生の五人でチームを組みいくつか賞を受賞しました。</p>
-          <Space space={1} margin />
-          <div
-            css={css`
-              width: max-content;
-              background: ${theme.colors.primary};
-              text-align: center;
-              border-radius: 20px;
-              font-size: 0.8rem;
-            `}
-          >
-            <p
-              css={css`
-                padding: 0.5rem;
-                line-height: 1;
-              `}
-            >
-              技術スタック
-            </p>
-          </div>
-        </Card>
+        <WorkdetealCard />
       </ContentContainer>
     </>
   )
