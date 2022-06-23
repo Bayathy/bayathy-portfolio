ab0caba99212f942b1c70731482bb451b5b77be4
Bayathy <misosoup0321@icloud.com>
2022-06-23 13:56:13 +0900

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+   reactStrictMode: true,
+   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.externals['node:fs'] = '@node/fs'
+      config.externals['node:path'] = '@node/path'
+      return config
+   }
 }
 
 module.exports = nextConfig
