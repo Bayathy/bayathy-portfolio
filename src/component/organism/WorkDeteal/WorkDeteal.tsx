b7a6e6b430d9889b5ef56184b5c29a0d8ed30155
Bayathy <misosoup0321@icloud.com>
2022-06-23 11:49:13 +0900

    create eslint ignore

--- src/component/organism/WorkDeteal/WorkDeteal.tsx
+++ src/component/organism/WorkDeteal/WorkDeteal.tsx
@@ -1,37 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { css, useTheme } from '@emotion/react'
 import { ContentContainer, Space, WorkTitle } from '../../atom'
 import { WorkdetealCard } from '../../molecular'
+import { getpostData } from '../../../lib/post'
 
 export const WorkDeteal: React.FC = () => {
-  const theme = useTheme()
+   const theme = useTheme()
 
-  return (
-    <>
-      <ContentContainer>
-        <div
-          css={css`
-            position: relative;
-            width: 70%;
-            margin: auto;
-            aspect-ratio: 16 /9;
-          `}
-        >
-          <Image
-            style={{ borderRadius: '10px' }}
-            src={'/Icon3.jpg'}
-            layout={'fill'}
-            objectFit={'cover'}
-          />
-        </div>
-        <Space space={3} margin />
-        <div>
-          <WorkTitle />
-        </div>
-        <Space space={2} margin />
-        <WorkdetealCard />
-      </ContentContainer>
-    </>
-  )
+   const [testtext, setText] = useState('')
+
+   useEffect(() => {
+      const titlePromise = getpostData('test')
+      titlePromise.then((data) => {
+         setText(data.title)
+      })
+   })
+
+   return (
+      <>
+         <ContentContainer>
+            <div
+               css={css`
+                  position: relative;
+                  width: 70%;
+                  margin: auto;
+                  aspect-ratio: 16 /9;
+               `}
+            >
+               <Image
+                  style={{ borderRadius: '10px' }}
+                  src={'/Icon3.jpg'}
+                  layout={'fill'}
+                  objectFit={'cover'}
+               />
+            </div>
+            <Space space={3} margin />
+            <div>
+               <WorkTitle />
+            </div>
+            <Space space={2} margin />
+            <WorkdetealCard />
+         </ContentContainer>
+      </>
+   )
 }
