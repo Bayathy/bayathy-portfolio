88b4e09ffb686f2a2242f466c469f6345e27284a
Bayathy <misosoup0321@icloud.com>
2022-06-24 01:37:01 +0900

    add getStaticPaths

--- src/pages/works/[id].tsx
+++ src/pages/works/[id].tsx
@@ -1,19 +1,29 @@
-import { getpostData } from '../../lib/post'
-import { GetStaticProps, NextPage } from 'next'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 import { WorksLayout } from '../../component/template'
 import { Workpostmatter } from '../../type/Workpost'
+import { getAllWorkPostId, getWorkPostData } from '../../lib/workpost'
 
 export type WorkProps = {
    data: Workpostmatter
 }
 
-export const getStaticProps: GetStaticProps<WorkProps> = async () => {
-   const data = await getpostData('test').then((data) => data)
+export const getStaticProps: GetStaticProps<WorkProps> = async ({ params }) => {
+   const id = params!.id
+
+   const data = await getWorkPostData(id as string).then((data) => data)
 
    return {
       props: {
          data
       }
+   }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+   const paths = await getAllWorkPostId()
+   return {
+      paths,
+      fallback: false
    }
 }
 
