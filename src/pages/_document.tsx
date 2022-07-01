222418130342c5f57dc613f3dbdf5c8b89dc1950
Bayathy <misosoup0321@icloud.com>
2022-07-01 23:55:14 +0900

    add Title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,25 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Anek+Malayalam:wght@500&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+   render() {
+      return (
+         <Html>
+            <Head>
+               <link rel="preconnect" href="https://fonts.googleapis.com" />
+               <link rel="preconnect" href="https://fonts.gstatic.com" />
+               <link
+                  href="https://fonts.googleapis.com/css2?family=Anek+Malayalam:wght@500&display=swap"
+                  rel="stylesheet"
+               />
+               <title>Bayathy&apos;s Portfolio</title>
+            </Head>
+            <body>
+               <Main />
+               <NextScript />
+            </body>
+         </Html>
+      )
+   }
 }
 
 export default MyDocument
