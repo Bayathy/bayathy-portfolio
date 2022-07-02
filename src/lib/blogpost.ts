71d357ecadfa142be1e488fd467143cff05247e8
Bayathy <misosoup0321@icloud.com>
2022-07-02 17:21:57 +0900

    create blogposts.ts

---
+++ src/lib/blogpost.ts
@@ -0,0 +1,42 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
+import { WorkPostmatter } from '../type/Workpost'
+
+const postdir = path.resolve(process.cwd(), 'blogposts')
+
+export async function getBlogPostData(id: string) {
+  const filepath = path.resolve(postdir, `${id}.md`)
+  const fileContents = fs.readFileSync(filepath, 'utf8')
+
+  const matterResult = matter(fileContents)
+  const processedContent = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+
+  const contentHtml = await processedContent
+    .process(matterResult.content)
+    .then((data) => data.toString())
+
+  const content = contentHtml.replace(/<p>|<\/p>/g, '')
+
+  const result = { content, ...matterResult.data }
+
+  return result as WorkPostmatter
+}
+
+export async function getAllBlogPostId() {
+  const fileNames = fs.readdirSync(postdir)
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, '')
+      }
+    }
+  })
+}
