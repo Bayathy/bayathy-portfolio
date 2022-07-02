5c8ba7715fed708a75cb8620761d046e1270a7ae
Bayathy <misosoup0321@icloud.com>
2022-07-02 19:00:38 +0900

    create Article title

--- blogposts/test.md
+++ blogposts/test.md
@@ -1,5 +1,5 @@
 ---
-title : "test"
+title : "ブログを書く"
 ---
 
 # mokuzi
--- src/component/template/ArticleLayout/ArticleLayout.tsx
+++ src/component/template/ArticleLayout/ArticleLayout.tsx
@@ -1,15 +1,42 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { BaseLayout } from '../BaseLayout'
 import { BlogPost } from '../../../type/Blogpost'
-import { ContentContainer } from '../../atom'
+import { ContentContainer, Space } from '../../atom'
+import { css, useTheme } from '@emotion/react'
 
 export const ArticleLayout: React.FC<BlogPost> = ({ data }) => {
-   useEffect(() => console.log(data.content))
+   const theme = useTheme()
 
    return (
       <BaseLayout>
          <ContentContainer>
-            <div dangerouslySetInnerHTML={{ __html: data.content }} />
+            <p
+               css={css`
+                  width: 90%;
+                  line-height: 1.2;
+                  font-weight: bold;
+                  margin: auto;
+                  font-size: 2rem;
+                  border-bottom: 2px solid ${theme.colors.text};
+                  @media (max-width: 480px) {
+                     font-size: 1.5rem;
+                  }
+               `}
+            >
+               {data.title}
+            </p>
+            <Space space={1} margin />
+            <div
+               css={css`
+                  width: 90%;
+                  margin: auto;
+
+                  h1 {
+                     font-size: 1.5rem;
+                  }
+               `}
+               dangerouslySetInnerHTML={{ __html: data.content }}
+            />
          </ContentContainer>
       </BaseLayout>
    )
