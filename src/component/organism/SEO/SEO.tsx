45ffabbaaa1abcfcbc2d24ab320e556f72adb684
Bayathy <misosoup0321@icloud.com>
2022-07-09 16:26:54 +0900

    test

--- src/component/organism/SEO/SEO.tsx
+++ src/component/organism/SEO/SEO.tsx
@@ -21,19 +21,19 @@ export const SEO: React.FC<SEOProperty> = ({
          <meta property="og:title" content={title} />
          <meta property="og:description" content={description} />
          <meta property="og:type" content="blog" />
-         <meta
-            property="og:url"
-            content={
-               url
-                  ? url
-                  : 'https://res.cloudinary.com/dff7r0iah/image/upload/v1657348630/ogp-default_fhypwx.png'
-            }
-         />
+         <meta property="og:url" content={url} />
          <meta property="og:image" content={image} />
          <meta property="og:site_name" content={title} />
          <meta name="twitter:card" content="summary_large_image" />
          <meta name="twitter:site" content="@Bayathy" />
-         <meta name="twitter:url" content={image} />
+         <meta
+            name="twitter:url"
+            content={
+               image
+                  ? image
+                  : 'https://res.cloudinary.com/dff7r0iah/image/upload/v1657348630/ogp-default_fhypwx.png'
+            }
+         />
          <meta name="twitter:title" content={title} />
          <meta name="twitter:description" content={description} />
          <meta name="twitter:image" content={image} />
