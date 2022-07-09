9eba1d92a9d308e85573075227a1c1b9f272f31b
Bayathy <misosoup0321@icloud.com>
2022-07-09 18:36:35 +0900

    update:write skillscard deteal

--- src/component/organism/About/About.tsx
+++ src/component/organism/About/About.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Space } from '../../atom'
 import { History, HistoryBar, SkillsCard } from '../../molecular'
-import { css } from '@emotion/react'
+import { css, useTheme } from '@emotion/react'
 import { popUp } from '../../../style/animation'
+import { Icon } from '@iconify/react'
 
 export const About: React.FC = () => {
    const datalist: History[] = [
@@ -11,6 +12,8 @@ export const About: React.FC = () => {
       { date: '2022/1', text: '技育ハッカソン出場' },
       { date: '2022/3', text: '鳥羽鈴鹿合同ハッカソン 百五銀行賞受賞' }
    ]
+
+   const theme = useTheme()
 
    return (
       <>
@@ -23,7 +26,67 @@ export const About: React.FC = () => {
             <HistoryBar datalist={datalist} />
             <Space space={2} margin />
             <SkillsCard>
-               <p>adfadf</p>
+               <p>
+                  鈴鹿高専の三年生です。フロントエンド、Webデザイン、企画に興味があります。
+                  <br />
+                  旅行やゲームが趣味です。
+               </p>
+               <div
+                  css={css`
+                     display: flex;
+                     justify-content: center;
+                     column-gap: 0.5rem;
+                  `}
+               >
+                  <a
+                     css={css`
+                        color: ${theme.colors.text};
+                        border: 2.5px solid ${theme.colors.text};
+                        display: flex;
+                        padding: 0.3rem;
+                        justify-content: center;
+                        align-content: center;
+                        width: max-content;
+                        border-radius: 50%;
+
+                        :hover {
+                           border: 2.5px solid ${theme.colors.secondary};
+                           color: ${theme.colors.secondary};
+                        }
+                     `}
+                     href="https://twitter.com/Kobath_dev"
+                  >
+                     <Icon
+                        height="1.5rem"
+                        width="1.5rem"
+                        icon="tabler:brand-twitter"
+                     />
+                  </a>
+                  <a
+                     css={css`
+                        color: ${theme.colors.text};
+                        border: 2.5px solid ${theme.colors.text};
+                        display: flex;
+                        padding: 0.3rem;
+                        justify-content: center;
+                        align-content: center;
+                        width: max-content;
+                        border-radius: 50%;
+
+                        :hover {
+                           border: 2.5px solid ${theme.colors.secondary};
+                           color: ${theme.colors.secondary};
+                        }
+                     `}
+                     href="https://github.com/Bayathy"
+                  >
+                     <Icon
+                        height="1.5rem"
+                        width="1.5rem"
+                        icon="tabler:brand-github"
+                     />
+                  </a>
+               </div>
             </SkillsCard>
          </div>
       </>
