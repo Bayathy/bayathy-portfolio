ce94b081adc41d8a1a2c694666299398371af4ab
Bayathy <misosoup0321@icloud.com>
2022-07-10 17:23:58 +0900

    update:restyle MeuTabBar

--- src/component/atom/Tabitembox/Tabitembox.tsx
+++ src/component/atom/Tabitembox/Tabitembox.tsx
@@ -2,43 +2,43 @@ import React from 'react'
 import { css, useTheme } from '@emotion/react'
 
 type Tabitem = {
-  name: string
-  isSlect: boolean
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  onClick?: Function
+   name: string
+   isSlect: boolean
+   // eslint-disable-next-line @typescript-eslint/ban-types
+   onClick?: Function
 }
 
 const Tabitembox: React.FC<Tabitem> = ({ name, isSlect, onClick }) => {
-  const theme = useTheme()
+   const theme = useTheme()
 
-  const borderStyle =
-    isSlect == true
-      ? css`
-          color: ${theme.colors.text};
-        `
-      : css`
-          color: ${theme.colors.secondary};
-        `
+   const borderStyle =
+      isSlect == true
+         ? css`
+              color: ${theme.colors.text};
+           `
+         : css`
+              color: ${theme.colors.primary};
+           `
 
-  return (
-    <div
-      css={css`
-        display: flex;
-        width: 30%;
-        align-items: center;
-        justify-content: center;
-        font-size: 1.2rem;
-        padding-top: 10px;
-        padding-bottom: 10px;
-        ${borderStyle}
-      `}
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      onClick={onClick}
-    >
-      <p>{name}</p>
-    </div>
-  )
+   return (
+      <div
+         css={css`
+            display: flex;
+            width: 30%;
+            align-items: center;
+            justify-content: center;
+            font-size: 1.2rem;
+            padding-top: 10px;
+            padding-bottom: 10px;
+            ${borderStyle}
+         `}
+         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+         // @ts-ignore
+         onClick={onClick}
+      >
+         <p>{name}</p>
+      </div>
+   )
 }
 
 export { Tabitembox }
