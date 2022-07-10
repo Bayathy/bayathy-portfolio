f026a54aea26dbba51d63b31d9af612b5a22a521
Bayathy <misosoup0321@icloud.com>
2022-07-10 17:19:02 +0900

    update:style WorkPanel

--- src/component/molecular/WorksPanel/WorksPanel.tsx
+++ src/component/molecular/WorksPanel/WorksPanel.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from '../../atom'
+import { Card, Space } from '../../atom'
 import { css, useTheme } from '@emotion/react'
 import Link from 'next/link'
 
@@ -8,33 +8,33 @@ type WorksPanel = {
    content: string
 }
 
-export const WorksPanel: React.FC<WorksPanel> = ({ title }) => {
+export const WorksPanel: React.FC<WorksPanel> = ({ title, content }) => {
    const theme = useTheme()
 
    return (
       <Card
          className={'Card'}
          style={css`
-           width: 80%;
+            width: 80%;
          `}
       >
          <div
             css={css`
-              background: ${theme.colors.extra};
-              border-radius: inherit;
-              padding: ${theme.padding.small};
+               background: ${theme.colors.extra};
+               border-radius: inherit;
+               padding: ${theme.padding.small};
             `}
          >
             <div>
                <Link href={`works/${title}`}>
                   <a
                      css={css`
-                       text-align: left;
-                       font-size: 1.4rem;
+                        text-align: left;
+                        font-size: 1.4rem;
 
-                       :hover {
-                         text-decoration: underline;
-                       }
+                        :hover {
+                           text-decoration: underline;
+                        }
                      `}
                   >
                      {title}
@@ -44,14 +44,37 @@ export const WorksPanel: React.FC<WorksPanel> = ({ title }) => {
             <div>
                <p
                   css={css`
-                    width: inherit;
-                    text-overflow: ellipsis;
-                    white-space: nowrap;
-                    overflow: hidden;
+                     width: inherit;
+                     text-overflow: ellipsis;
+                     white-space: nowrap;
+                     overflow: hidden;
                   `}
                >
-
+                  {content}
                </p>
+            </div>
+            <Space space={1} margin />
+            <div
+               css={css`
+                  display: flex;
+                  justify-content: end;
+                  padding-right: 0.5rem;
+               `}
+            >
+               <a
+                  css={css`
+                     border: 2px solid ${theme.colors.text};
+                     padding: 0.2rem 0.5rem;
+                     border-radius: 10px;
+
+                     :hover {
+                        border: 2px solid ${theme.colors.secondary};
+                        color: ${theme.colors.secondary};
+                     }
+                  `}
+               >
+                  more
+               </a>
             </div>
          </div>
       </Card>
