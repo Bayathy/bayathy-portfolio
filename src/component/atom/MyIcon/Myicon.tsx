f84ce4a9d42a05e9b345de5322cac91e704e910f
Bayathy <misosoup0321@icloud.com>
2022-07-15 01:03:32 +0900

    update:add placeholder at MyIcon

--- src/component/atom/MyIcon/Myicon.tsx
+++ src/component/atom/MyIcon/Myicon.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { css } from '@emotion/react'
 import Image from 'next/image'
-import Imagesrc from './Icon3.jpg'
 
 export const Myicon: React.FC = () => {
    return (
@@ -14,11 +13,13 @@ export const Myicon: React.FC = () => {
          `}
       >
          <Image
-            src={Imagesrc}
+            src={'/Icon3.jpg'}
             layout={'fill'}
             alt={'My Icon'}
             style={{ borderRadius: 999, zIndex: 1 }}
-            priority={true}
+            priority
+            placeholder="blur"
+            blurDataURL={'/Icon3.jpg'}
          />
       </div>
    )
